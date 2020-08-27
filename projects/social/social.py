68b9d84cbe12c08516bace592e09724e9ff8f51e
74ultra <r.barnhill@protonmail.com>
2020-08-27 12:05:01 -0700

    MVP met

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,10 @@
+import random
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -45,8 +49,23 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(0, num_users):
+            self.add_user(f'User {i+1}')
 
         # Create friendships
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id+1):
+                possible_friendships.append((user_id, friend_id))
+
+        random.shuffle(possible_friendships)
+        num_friendships = num_users * avg_friendships // 2
+        count = 0
+        for i in range(0, num_friendships):
+            friendship = possible_friendships[i]
+            count += 1
+            self.add_friendship(friendship[0], friendship[1])
+        print(count)
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,14 +76,26 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        queue = [user_id]
+        result = []
         visited = {}  # Note that this is a dictionary, not a set
+
+        visited[user_id] = [user_id]
+        while len(queue) > 0:
+            current_vertex = queue.pop(0)
+            result.append(current_vertex)
+            for i in self.friendships[current_vertex]:
+                if i not in visited:
+                    visited[i] = visited[current_vertex] + [i]
+                    queue.append(i)
+
         # !!!! IMPLEMENT ME
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(100, 10)
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
